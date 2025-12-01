3ae6865ebef5fecc92e620d04e643166c47446fd
Andres Osorio <af.osorio1341@gmail.com>
2025-12-01 15:58:45 +0100

    add initial implementation to query all the entities and query unused entities

---
+++ src/args.rs
@@ -0,0 +1,38 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct NxalyzerArgs {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    /// Queries different types of entities in a nx project
+    QueryAll(QueryAllArgs),
+    /// Queries different types of entities in a nx project
+    Query(QueryArgs),
+    /// Lists all unused entities in the nx project
+    Unused(UnusedArgs),
+}
+
+#[derive(Args, Debug)]
+pub struct QueryArgs {
+    /// Path to the root of the nx project
+    pub path: String,
+    /// Query string to filter entities by
+    pub query: String,
+}
+
+#[derive(Args, Debug)]
+pub struct QueryAllArgs {
+    /// Path to the root of the nx project
+    pub path: String,
+}
+
+#[derive(Args, Debug)]
+pub struct UnusedArgs {
+    /// Path to the root of the nx project
+    pub path: String,
+}
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+mod args;
+
+use std::path::Path;
+
+use anyhow::{Context, Result};
+use args::{Commands, NxalyzerArgs};
+use clap::Parser;
+
+fn canonicalize_path(path_str: &str) -> Result<std::path::PathBuf> {
+    let path = Path::new(path_str);
+    path.canonicalize()
+        .with_context(|| format!("Unable to resolve path: {}", path_str))
+}
+
+fn main() -> Result<()> {
+    let cli = NxalyzerArgs::parse();
+
+    match &cli.command {
+        Commands::QueryAll(args) => {
+            let path = canonicalize_path(&args.path)?;
+
+            nxalyzer::query_all(&path)
+                .with_context(|| format!("Unable to query in path: {}", path.display()))?
+        }
+        Commands::Query(args) => {
+            let path = canonicalize_path(&args.path)?;
+
+            nxalyzer::query(&path, &args.query)
+                .with_context(|| format!("Unable to query in path: {}", path.display()))?
+        }
+        Commands::Unused(args) => {
+            let path = canonicalize_path(&args.path)?;
+
+            nxalyzer::unused(&path)
+                .with_context(|| format!("Unable to find unused entities in path: {}", path.display()))?
+        }
+    }
+
+    Ok(())
+}
